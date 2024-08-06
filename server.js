bdc7c819816c8046c1c713d27b8d36c351f44ed0
nop990 <62253708+nop990@users.noreply.github.com>
2024-08-05 23:39:53 -0400

    added port listener

--- server.js
+++ server.js
@@ -32,4 +32,7 @@ app.post('/api/format', (req, res) => {
         });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
