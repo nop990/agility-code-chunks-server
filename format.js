cf20b9b1929107a4eeaa19a27820ac9ed2542e47
nop990 <62253708+nop990@users.noreply.github.com>
2024-08-05 23:14:47 -0400

    init

---
+++ format.js
@@ -0,0 +1,40 @@
+const prettier = require('prettier');
+const typescript = require('prettier/plugins/typescript');
+const babel = require('prettier/plugins/babel');
+const estree = require('prettier/plugins/estree');
+const html = require('prettier/plugins/html');
+const markdown = require('prettier/plugins/markdown');
+const postcss = require('prettier/plugins/postcss');
+const yaml = require('prettier/plugins/yaml');
+
+async function formatCode(code, language) {
+    console.log("Formatting code:", code, language);
+    try {
+        let parser = language;
+        let plugins = [typescript, babel, estree, html, markdown, postcss, yaml];
+
+        if (language === 'java') {
+            // plugins.push(javaPlugin);
+        }
+
+        code = await prettier.format(code, {
+            organizeImportsSkipDestructiveCodeActions: true,
+            parser: parser,
+            plugins: [typescript, babel, estree, html, markdown, postcss, yaml, 'prettier-plugin-java'],
+            tabWidth: 4,
+            useTabs: true,
+            semi: true,
+            singleQuote: false,
+            trailingComma: "none",
+            bracketSpacing: true,
+            arrowParens: "always"
+        });
+
+        return code;
+    } catch (error) {
+        console.error("Error formatting code:", error);
+        return code;
+    }
+}
+
+module.exports = { formatCode };
---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+const format = require('./format.js');
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Root endpoint
+app.get('/health', (req, res) => {
+    res.send('Agility Code Chunks Server UP!');
+});
+
+app.post('/api/format', (req, res) => {
+    const { code, language } = req.body;
+    if (!code || !language) {
+        return res.status(400).send('Code and language are required.');
+    }
+    format.formatCode(code, language)
+        .then((formattedCode) => {
+            if (!formattedCode) {
+                return res.status(500).send('Formatting failed.');
+            }
+            res.send(formattedCode);
+        })
+        .catch((error) => {
+            console.error('Error formatting code:', error);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
